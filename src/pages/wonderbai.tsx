6cb54952de61b7856adf878f75af265c9a2c9cea
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2022-08-07 15:40:40 +0200

    Update wonderbai.tsx

--- src/pages/wonderbai.tsx
+++ src/pages/wonderbai.tsx
@@ -2,34 +2,37 @@
 
 import React from 'react'
 import { Link } from 'gatsby'
+import Layout from "../components/layout"
 import { ModalRoutingContext } from 'gatsby-plugin-modal-routing'
 
 import talent from '../data/talent';
 
 const ModalExamplePage = () => (
-  <ModalRoutingContext.Consumer>
-    {({ modal, closeTo }) => (
-      <div>
-        {modal ? (
-          <Link to={closeTo}>
-            Close
-          </Link>
-        ) : (
-          <header>
-            <h1>
-              {talent[0].text}
-            </h1>
+  <Layout>
+    <ModalRoutingContext.Consumer>
+      {({ modal, closeTo }) => (
+        <div>
+          {modal ? (
+            <Link to={closeTo}>
+              Close
+            </Link>
+          ) : (
+            <header>
+              <h1>
+                {talent[0].text}
+              </h1>
 
-           <a href={talent[0].stream}> {talent[0].stream}</a>
-          </header>
-        )}
+            <p><a href={talent[0].stream}> {talent[0].stream}</a>
+            </header>
+          )}
 
-        <h2>Modal Page</h2>
+          <h2>Modal Page</h2>
 
-        <Link to="/">Go back to the homepage</Link>
-      </div>
-    )}
-  </ModalRoutingContext.Consumer>
+          <Link to="/">Go back to the homepage</Link>
+        </div>
+      )}
+    </ModalRoutingContext.Consumer>
+  </Layout>
 )
 
 export default ModalExamplePage
