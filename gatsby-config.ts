a710a448e384673e8af6ccec5e771b314d0df50b
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2022-08-10 15:08:31 +0200

    fav icon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -16,6 +16,12 @@ const config: GatsbyConfig = {
       "path": "./src/pages/"
     },
     __key: "pages"
+  },
+  {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      icon: 'src/images/icon.png',
+    },
   }]
 };
 
