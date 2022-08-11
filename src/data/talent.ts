549a5298e144a2b7bad26bb99b5a513a411b90bd
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2022-08-11 09:45:26 +0200

    Update talent.ts

--- src/data/talent.ts
+++ src/data/talent.ts
@@ -5,27 +5,35 @@ const talent = [
       image: "/talent/wonderbai.jpg",
       name: "JENS SÆTTER-LASSEN",
       stream: "https://twitch.tv/wonderbai",
-      description: "Jens Sætter-Lassen, alias Wonderbai. Dansk skuespiller og online streamer. Wonderbai er kendt som ikke alene en af de største streamere i Danmark, men også en stream der går forrest på vigtige parametre som online-opførsel og god kommunikation. Nerven i det hele er gaming, hygge, god musik, underholdning, talkshows, røverhistorier og en nærmest familiær stemning. Jens indledte sin skuespillerkarriere som barn i bl.a. Alletiders Nisse og i julekalenderen Olsenbandens Første Kup, hvor han havde hovedrollen som den unge Egon Olsen. Siden har det udviklet sig til adskillige roller i både tv-serier, film og teatersstykker. Derudover har Jens lagt stemme til roller i et hav af tegnefilm samt utallige reklamespeaks. Jens er specialist i at formidle, underholde og skabe eksponering. Ved siden af sin karriere som skuespiller, har Jens de seneste år skabt en eksplosiv vækst som streamer. Streamen har manifesteret sig som en af Danmarks største på platformen Twitch, og flere faste sponsorer er allerede tilsluttet projektet. Der er tale om stor og tiltagende popularitet og massiv eksponering; og det går kun en vej."
+      instagram: 'https://www.instagram.com/jenssaetterlassen/',
+      // presskit: ''
     },
     {
       text: "agge",
       url: "/agge/",
       image: "/talent/agge.jpg",
+      name: 'Agge Kaadt Trandum',
+      stream: 'https://www.twitch.tv/agge'
     },
     {
       text: "BRIANFROMDENMARK",
-      url: "/BRIANFROMDENMARK/",
+      url: "/brianfromdenmark/",
       image: "/talent/brian.jpg",
+      name: 'Brian Ikast Sørensen'
     },
     {
       text: "Zrool",
       url: "/Zrool/",
       image: "/talent/zrool.jpg",
+      name: 'Mikkel Uldahl Nielsen',
+      stream: 'https://www.twitch.tv/Zrool'
     },
     {
       text: "JK",
       url: "/JK/",
       image: "/talent/jk.jpg",
+      name: 'Jakob Kristensen',
+      stream: 'https://www.twitch.tv/JKFIFA'
     },
 ];
 
