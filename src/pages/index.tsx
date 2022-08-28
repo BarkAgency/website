f60febae9949cfbd73f9e60d61be6337dc42a4a7
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2022-08-28 13:25:59 +0200

    Add Snap to sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,319 +1,438 @@
-import * as React from "react"
-import logo from "../images/bark.svg";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
-import Layout from "../components/layout"
+import * as React from 'react';
+import { useEffect } from 'react';
+import logo from '../images/bark.svg';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import Layout from '../components/layout';
 import { Link } from 'gatsby';
+
+import PanelSnap from 'panelsnap'
 import { Helmet } from 'react-helmet';
 
-import Copyright from "../components/copyright";
-import { AnchorLink } from "gatsby-plugin-anchor-links";
- 
+import Copyright from '../components/copyright';
+import { AnchorLink } from 'gatsby-plugin-anchor-links';
+
 import talent from '../data/talent';
 
 // styles
 const pageStyles = {
-  color: "#232129",
-  backgroundColor: "#000000",
-}
+	color: '#232129',
+	backgroundColor: '#000000',
+};
 const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  fontSize: 53,
-  lineHeight: 1,
-  fontWeight: 400
-}
+	marginTop: 0,
+	marginBottom: 64,
+	fontSize: 45,
+	lineHeight: 1,
+	fontWeight: 400,
+  textAlign: 'center'
+};
 const headingAccentStyles = {
-  color: "#663399",
-}
+	color: '#663399',
+};
 const paragraphStyles = {
-  marginBottom: 48,
-}
+	marginBottom: 48,
+};
 const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+	color: '#8A6534',
+	padding: 4,
+	backgroundColor: '#FFF4DB',
+	fontSize: '1.25rem',
+	borderRadius: 4,
+};
 const listStyles = {
-  paddingLeft: 0,
-  display: 'flex',
-  flexDirection: 'row',
-  margin: 0,
-  alignItems: 'center',
-  flexWrap: 'wrap'
-}
+	paddingLeft: 0,
+	display: 'flex',
+	flexDirection: 'row',
+	margin: 0,
+	alignItems: 'center',
+	flexWrap: 'wrap',
+};
 const menuStyles = {
-  paddingLeft: 0,
-  display: 'flex',
-  justifyContent: 'space-between',
-  marginTop: 0
-}
+	paddingLeft: 0,
+	display: 'flex',
+	justifyContent: 'space-between',
+	marginTop: 0,
+};
 const listItemStyles = {
-  width: 199,
-  marginBottom: 30,
-  listStyleType: "none",
-  display: `inline-block`,
-  margin: '.5rem',
-}
+	width: 199,
+	marginBottom: 30,
+	listStyleType: 'none',
+	display: `inline-block`,
+	margin: '.5rem',
+};
 const listItemLinkStyles = {
-  color: 'rgba(255, 255, 255, 0.85)' ,
-  textDecoration: 'none',
-  textTransform: 'uppercase',
-  fontSize: '10px'
-}
+	color: 'rgba(255, 255, 255, 0.85)',
+	textDecoration: 'none',
+	textTransform: 'uppercase',
+	fontSize: '10px',
+};
 
 const linkStyle = {
-  color: "rgba(0, 0, 0, 0.85)",
-  fontWeight: "normal",
-  fontSize: 16,
-  textTransform: 'uppercase',
-  verticalAlign: "5%",
-  textDecoration: 'none'
-}
+	color: 'rgba(0, 0, 0, 0.85)',
+	fontWeight: 'normal',
+	fontSize: 16,
+	textTransform: 'uppercase',
+	verticalAlign: '5%',
+	textDecoration: 'none',
+};
 const linkImgStyle = {
-  maxWidth: '100%',
-  minWidth: 170,
-  maxHeight: '400px',
-  transition: 'all .2s ease-in-out',
-  '&:hover': {
-    transform: 'scale(1.1)'
-  }
-}
+	maxWidth: '100%',
+	minWidth: 170,
+	maxHeight: '400px',
+	transition: 'all .2s ease-in-out',
+	'&:hover': {
+		transform: 'scale(1.1)',
+	},
+};
 
 const menuLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
+	...linkStyle,
+	listStyleType: 'none',
+	display: `inline-block`,
+	marginBottom: 24,
+	marginRight: 12,
+};
 const menuLinkAStyle = {
-  color: "rgba(0, 0, 0, 0.85)",
-  fontSize: '11px',
-  fontWeight: "normal",
-  textTransform: 'uppercase',
-  verticalAlign: "5%",
-  textDecoration: 'none',
-}
+	color: 'rgba(0, 0, 0, 0.85)',
+	fontSize: '11px',
+	fontWeight: 'normal',
+	textTransform: 'uppercase',
+	verticalAlign: '5%',
+	textDecoration: 'none',
+};
 
 const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+	color: '#232129',
+	fontSize: 14,
+	marginTop: 10,
+	marginBottom: 0,
+	lineHeight: 1.25,
+};
 
 const logoStyles = {
-  width: "600px",
-  maxWidth: "60%",
-  margin: 'auto',
-  display: 'block'
-}
+	width: '600px',
+	maxWidth: '60%',
+	margin: 'auto',
+	display: 'block',
+};
 
 const viewZoneStyles = {
-  minHeight: "100vh",
-  //padding: 40,
-  margin: 'auto',
-  display: 'block',
-}
+	minHeight: '100vh',
+	margin: 'auto',
+	display: 'block',
+};
 
 const docLinks = [
-  {
-    text: "Talent",
-    url: "talent",
-    color: "#8954A8",
-  },
-  {
-    text: "Cases",
-    url: "cases",
-    color: "#8954A8",
-  },
-  {
-    text: "Kontakt",
-    url: "kontakt",
-    color: "#8954A8",
-  }
-]
+	{
+		text: 'Talent',
+		url: 'talent',
+		color: '#8954A8',
+	},
+	{
+		text: 'Cases',
+		url: 'cases',
+		color: '#8954A8',
+	},
+	{
+		text: 'Kontakt',
+		url: 'kontakt',
+		color: '#8954A8',
+	},
+];
 
 const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+	color: '#fff',
+	backgroundColor: '#088413',
+	border: '1px solid #088413',
+	fontSize: 11,
+	fontWeight: 'bold',
+	letterSpacing: 1,
+	borderRadius: 4,
+	padding: '4px 6px',
+	display: 'inline-block',
+	position: 'relative' as 'relative',
+	top: -2,
+	marginLeft: 10,
+	lineHeight: 1,
+};
 
 // markup
 const IndexPage = () => {
-
-    // window.addEventListener(`scroll`, () => {
-
-    //   console.log('scroll')
-    // });
-
-  return (
-    <Layout>
-      <main style={pageStyles}>
-
-        <Helmet>
-          <title>Bark Agency</title>
-        </Helmet>
-      
-        <section style={{
-          ...viewZoneStyles,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}>
-          <img style={logoStyles} src={logo} alt="Logo" />
-        </section>
-
-        <section style={{ ...viewZoneStyles, background: '#ffffff' }}>
-          <section style={{
-              margin: 'auto',
-              padding: 26,
-              textAlign: 'left',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'space-between',
-              height: '100vh',
-            }}>
+	useEffect(() => {
 
 
-            <ul style={menuStyles}>
-              {docLinks.map(menuLink => (
-                <li style={menuLinkStyle} key={menuLink.url}>
-                  <AnchorLink gatsbyLinkProps={{ style: menuLinkAStyle }} to={`#${menuLink.url}`} title={menuLink.text} />
-                </li>
-              ))}
-            </ul>
-
-            <h2 style={headingStyles}>
-              <p>Bark Agency leverer live interactive marketing. Vi kommercialiserer danske live streamere, og får dit brand og budskab igennem til en unik målgruppe med stor effekt.</p>
-            </h2>
-          </section>
-    
-        </section>
-    
-        <section id="talent" style={{ ...viewZoneStyles,  display: 'flex' }}>
-          <section style={{
-            width: '70%',
-            height: '100%',
-            margin: 'auto',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-            <ul style={{...listStyles}}>
-              {talent.map(talent => (
-                <li key={talent.url} style={{ ...listItemStyles }}>
-                  <span>
-                    <Link
-                      style={ listItemLinkStyles }
-                      state={{
-                        modal: true
-                      }}
-                      to={`${talent.url}`}
-                    >
-                      <img style={linkImgStyle}  src={talent.image}  alt={talent.text}/>
-                      <p>{talent.text}</p>
-                    </Link>
-                    {talent.badge && (
-                      <span style={badgeStyle} aria-label="New Badge">
-                        NY!
-                      </span>
-                    )}
-                    
-                  </span>
-                </li>
-              ))}
-            </ul>
-          </section>
-        </section>
-
-        <section id="kontakt" style={{ ...viewZoneStyles, background: '#ffffff' }}>
-          <div style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              flexDirection: 'column',
-
-              height: '100vh',
-            }}>
-
-   
-          <section style={{
-            width: '70%',
-            margin: 'auto',
-            textAlign: 'center',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            fontSize: '23px',
-          }}>
-              <div>
-                <p>Bark Agency<br />
-                  <a style={{ textDecoration: 'none', color: 'rgba(0, 0, 0, 0.85)' }} href="mailto:hello@barkagency.dk">hello@barkagency.dk</a><br />
-                  <a style={{ textDecoration: 'none', color: 'rgba(0, 0, 0, 0.85)' }} href="tel:+4523443936">+45 23443936</a>
-                </p>
-                <p>
-                  CVR 43380273<br />
-                  Vilhelmsro 670<br />
-                  Fredensborg, Danmark<br />
-                </p>
-              </div>
-           
-
-                 
+    if (typeof window !== 'undefined') {
+      new PanelSnap({
+        panelSelector: 'section[id]'
+      });
+    }
 
 
 
-          </section>
+		// var lastScrollTop = 0;
+		// const sections = Array.from(document.querySelectorAll('section[id]'));
 
-          <section style={{
-                    textAlign: 'center',  
-                    background: '#fff',
-                    fontSize: '0.7rem',
-                    padding: 26,
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                  }}>
+		// const handleScroll = (event) => {
+		// 	var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
 
-                    <Link style={ {
-                        color: '#000',
-                        textTransform: 'uppercase',
-                        textDecoration: 'none'
-                      } } to={`/produkt`}>
-                      Produkt
-                    </Link>
-                  
-                    <Copyright />
+		// 	//Find next
+		// 	const prevSectionElement = sections.find((sec) => {
+		// 		const elePos = sec.getBoundingClientRect().y + window.scrollY;
+		// 		console.log(sec.id, sec.getBoundingClientRect().y + window.scrollY);
 
-                    <AnchorLink gatsbyLinkProps={{ style: {
-                        color: '#000',
-                        textTransform: 'uppercase',
-                        textDecoration: 'none'
-                      } }} to={`#talent`} title={"talent"} />
+		// 		return window.scrollY > elePos;
+		// 	});
+		// 	const nextSectionElement = sections.find((sec) => {
+		// 		const elePos = sec.getBoundingClientRect().y + window.scrollY;
+		// 		console.log(sec.id, sec.getBoundingClientRect().y + window.scrollY);
 
-                  </section>
-                  </div>
-     
-        </section>
-      </main>
-    </Layout>
-  )
-}
+		// 		return window.scrollY < elePos;
+		// 	});
 
-export default IndexPage
+		// 	if (!nextSectionElement) return;
+		// 	const nextPos =
+		// 		nextSectionElement.getBoundingClientRect().y + window.scrollY;
+
+		// 	if (st > lastScrollTop) {
+		// 		// downscroll code
+		// 		console.log(
+		// 			'scrolling down',
+		// 			st,
+		// 			prevSectionElement,
+		// 			nextSectionElement
+		// 		);
+
+		// 		if (st - lastScrollTop > 200) {
+		// 			nextSectionElement.scrollIntoView({
+		// 				behavior: 'smooth',
+		// 			});
+		// 		}
+		// 	} else {
+		// 		// upscroll code
+		// 		console.log('scrolling up');
+		// 	}
+		// 	lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+
+		// 	console.log('we are at', st);
+		// 	console.log('next is at', nextPos);
+
+		// 	// hvis sidste element er mere end 20 pc væk scroll til næste.
+		// 	//if (st )
+
+		// 	// allan && allan.scrollIntoView({
+		// 	//     behavior: 'smooth'
+		// 	// });
+
+		// 	console.log('window.scrollY', window.scrollY, nextSectionElement);
+		// };
+
+		// //   document.querySelector('#kontakt').scrollIntoView({
+		// //     behavior: 'smooth'
+		// // });
+
+		// window.addEventListener('scroll', handleScroll);
+
+		// return () => {
+		// 	window.removeEventListener('scroll', handleScroll);
+		// };
+	}, []);
+
+	return (
+		<Layout>
+			<main style={pageStyles}>
+				<Helmet>
+					<title>Bark Agency</title>
+				</Helmet>
+
+				<section id='logo'
+					style={{
+						...viewZoneStyles,
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<img style={logoStyles} src={logo} alt="Logo" />
+				</section>
+
+				<section id="om" style={{ ...viewZoneStyles, background: '#ffffff' }}>
+					<section
+						style={{
+							margin: 'auto',
+							padding: 26,
+							textAlign: 'left',
+							display: 'flex',
+							flexDirection: 'column',
+							justifyContent: 'space-between',
+							height: '100vh',
+						}}
+					>
+						<ul style={menuStyles}>
+							{docLinks.map((menuLink) => (
+								<li style={menuLinkStyle} key={menuLink.url}>
+									<AnchorLink
+										gatsbyLinkProps={{ style: menuLinkAStyle }}
+										to={`#${menuLink.url}`}
+										title={menuLink.text}
+									/>
+								</li>
+							))}
+						</ul>
+
+						<h2 style={headingStyles}>
+							<p>
+								Bark Agency leverer live interactive marketing. Vi
+								kommercialiserer danske live streamere, og får dit brand og
+								budskab igennem til en unik målgruppe med stor effekt.
+							</p>
+						</h2>
+            <div></div>
+					</section>
+				</section>
+
+				<section id="talent" style={{ ...viewZoneStyles, display: 'flex' }}>
+					<section
+						style={{
+							width: '70%',
+							height: '100%',
+							margin: 'auto',
+							display: 'flex',
+							flexDirection: 'column',
+							justifyContent: 'center',
+							alignItems: 'center',
+						}}
+					>
+						<ul style={{ ...listStyles }}>
+							{talent.map((talent) => (
+								<li key={talent.url} style={{ ...listItemStyles }}>
+									<span>
+										<Link
+											style={listItemLinkStyles}
+											state={{
+												modal: true,
+											}}
+											to={`${talent.url}`}
+										>
+											<img
+												style={linkImgStyle}
+												src={talent.image}
+												alt={talent.text}
+											/>
+											<p>{talent.text}</p>
+										</Link>
+										{talent.badge && (
+											<span style={badgeStyle} aria-label="New Badge">
+												NY!
+											</span>
+										)}
+									</span>
+								</li>
+							))}
+						</ul>
+					</section>
+				</section>
+
+				<section
+					id="kontakt"
+					style={{ ...viewZoneStyles, background: '#ffffff' }}
+				>
+					<div
+						style={{
+							display: 'flex',
+							justifyContent: 'space-between',
+							flexDirection: 'column',
+
+							height: '100vh',
+						}}
+					>
+						<section
+							style={{
+								width: '70%',
+								margin: 'auto',
+								textAlign: 'center',
+								display: 'flex',
+								flexDirection: 'column',
+								justifyContent: 'center',
+								alignItems: 'center',
+								fontSize: '23px',
+							}}
+						>
+							<div>
+								<p>
+									Bark Agency
+									<br />
+									<a
+										style={{
+											textDecoration: 'none',
+											color: 'rgba(0, 0, 0, 0.85)',
+										}}
+										href="mailto:hello@barkagency.dk"
+									>
+										hello@barkagency.dk
+									</a>
+									<br />
+									<a
+										style={{
+											textDecoration: 'none',
+											color: 'rgba(0, 0, 0, 0.85)',
+										}}
+										href="tel:+4523443936"
+									>
+										+45 23443936
+									</a>
+								</p>
+								<p>
+									CVR 43380273
+									<br />
+									Vilhelmsro 670
+									<br />
+									Fredensborg, Danmark
+									<br />
+								</p>
+							</div>
+						</section>
+
+						<section
+							style={{
+								textAlign: 'center',
+								background: '#fff',
+								fontSize: '0.7rem',
+								padding: 26,
+								display: 'flex',
+								justifyContent: 'space-between',
+							}}
+						>
+							<Link
+								style={{
+									color: '#000',
+									textTransform: 'uppercase',
+									textDecoration: 'none',
+								}}
+								to={`/produkt`}
+							>
+								Produkt
+							</Link>
+
+							<Copyright />
+
+							<AnchorLink
+								gatsbyLinkProps={{
+									style: {
+										color: '#000',
+										textTransform: 'uppercase',
+										textDecoration: 'none',
+									},
+								}}
+								to={`#talent`}
+								title={'talent'}
+							/>
+						</section>
+					</div>
+				</section>
+			</main>
+		</Layout>
+	);
+};
+
+export default IndexPage;
