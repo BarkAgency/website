049cd924b2d4f8ee496202f190151870c49f5547
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2022-08-30 18:25:03 +0200

    Update talent.ts

--- src/data/talent.ts
+++ src/data/talent.ts
@@ -1,59 +1,60 @@
 const talent = [
-    {
-      text: "Wonderbai",
-      url: "/wonderbai/",
-      image: "/talent/wonderbai.jpg",
-      name: "JENS SÆTTER-LASSEN",
-      stream: "https://twitch.tv/wonderbai",
-      instagram: 'https://www.instagram.com/jenssaetterlassen/',
-      youtube: 'https://www.youtube.com/c/wonderbai',
-      presskit: 'https://www.dropbox.com/s/m51s0hkyg1529mk/Wonderbai%20Sponsorkoncept.pdf?dl=0'
-    },
-    {
-      text: "agge",
-      url: "/agge/",
-      image: "/talent/agge.jpg",
-      name: 'Agge',
-      stream: 'https://www.twitch.tv/agge',
-      instagram: 'https://www.instagram.com/HakkeAgge/',
-      youtube: 'https://www.youtube.com/c/AggeHakke'
-    },
-    {
-      text: "BRIANFROMDENMARK",
-      url: "/brianfromdenmark/",
-      image: "/talent/brian.jpg",
-      name: 'Brian Ikast Sørensen',
-      stream: 'https://www.twitch.tv/brianfromdenmark',
-      instagram: 'https://www.instagram.com/brianowitsz/',
-      youtube: 'https://www.youtube.com/BrianFromDenmark'
-    },
-    {
-      text: "Zrool",
-      url: "/Zrool/",
-      image: "/talent/zrool.jpg",
-      name: 'Mikkel Uldahl Nielsen',
-      stream: 'https://www.twitch.tv/Zrool',
-      instragram: 'https://www.instagram.com/therealzrool/',
-      youtube: 'https://www.youtube.com/c/Zrool'
-    },
-    {
-      text: "JK",
-      url: "/JK/",
-      image: "/talent/jk.jpg",
-      name: 'Jakob Kristensen',
-      stream: 'https://www.twitch.tv/JKFIFA',
-      instagram: 'https://www.instagram.com/jakob_kristensen97/',
-      youtube: 'https://www.youtube.com/c/JKFIFA/'
-    },
-    {
-      text: "ELLAH",
-      url: "/ELLAH/",
-      image: "/talent/ellahdk.jpg",
-      name: 'Benjamin Juhl Kristensen',
-      stream: 'https://www.twitch.tv/ellahdk',
-      instagram: 'https://www.instagram.com/ellahgram',
-      youtube: 'https://www.youtube.com/c/ellahdk/'
-    },
+	{
+		text: 'Wonderbai',
+		url: '/wonderbai/',
+		image: '/talent/wonderbai.jpg',
+		name: 'JENS SÆTTER-LASSEN',
+		stream: 'https://twitch.tv/wonderbai',
+		instagram: 'https://www.instagram.com/jenssaetterlassen/',
+		youtube: 'https://www.youtube.com/c/wonderbai',
+		presskit:
+			'https://www.dropbox.com/s/m51s0hkyg1529mk/Wonderbai%20Sponsorkoncept.pdf?dl=0',
+	},
+	{
+		text: 'agge',
+		url: '/agge/',
+		image: '/talent/agge.jpg',
+		name: 'Agge',
+		stream: 'https://www.twitch.tv/agge',
+		instagram: 'https://www.instagram.com/HakkeAgge/',
+		youtube: 'https://www.youtube.com/c/AggeHakke',
+	},
+	{
+		text: 'BRIANFROMDENMARK',
+		url: '/brianfromdenmark/',
+		image: '/talent/brian.jpg',
+		name: 'Brian Ikast Sørensen',
+		stream: 'https://www.twitch.tv/brianfromdenmark',
+		instagram: 'https://www.instagram.com/brianowitsz/',
+		youtube: 'https://www.youtube.com/BrianFromDenmark',
+	},
+	{
+		text: 'Zrool',
+		url: '/Zrool/',
+		image: '/talent/zrool.jpg',
+		name: 'Mikkel Uldahl Nielsen',
+		stream: 'https://www.twitch.tv/Zrool',
+		instagram: 'https://www.instagram.com/therealzrool/',
+		youtube: 'https://www.youtube.com/c/Zrool',
+	},
+	{
+		text: 'JK',
+		url: '/JK/',
+		image: '/talent/jk.jpg',
+		name: 'Jakob Kristensen',
+		stream: 'https://www.twitch.tv/JKFIFA',
+		instagram: 'https://www.instagram.com/jakob_kristensen97/',
+		youtube: 'https://www.youtube.com/c/JKFIFA/',
+	},
+	{
+		text: 'ELLAH',
+		url: '/ELLAH/',
+		image: '/talent/ellahdk.jpg',
+		name: 'Benjamin Juhl Kristensen',
+		stream: 'https://www.twitch.tv/ellahdk',
+		instagram: 'https://www.instagram.com/ellahgram',
+		youtube: 'https://www.youtube.com/c/ellahdk/',
+	},
 ];
 
 export default talent;
