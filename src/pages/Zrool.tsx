06b4c845ad7cbce2dbf07268dc0e39dd48a42a0c
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2022-08-30 18:22:07 +0200

    Update Zrool.tsx

--- src/pages/Zrool.tsx
+++ src/pages/Zrool.tsx
@@ -1,10 +1,10 @@
 // pages/modal-example.js
 
-import React from 'react'
+import React from 'react';
 import { Link } from 'gatsby';
-import { AnchorLink } from "gatsby-plugin-anchor-links";
-import Layout from "../components/layoutModal"
-import { ModalRoutingContext } from 'gatsby-plugin-modal-routing'
+import { AnchorLink } from 'gatsby-plugin-anchor-links';
+import Layout from '../components/layoutModal';
+import { ModalRoutingContext } from 'gatsby-plugin-modal-routing';
 import { Helmet } from 'react-helmet';
 
 import talent from '../data/talent';
@@ -14,19 +14,19 @@ import TalentPage from '../components/talentPage';
 const TALENT_INDEX = 3;
 
 const ModalExamplePage = () => (
-  <Layout>
-    <TalentPage
-      {... {
-        title: talent[TALENT_INDEX].text,
-        name: talent[TALENT_INDEX].name,
-        image: talent[TALENT_INDEX].image,
-        stream: talent[TALENT_INDEX].stream,
-        instagram: talent[TALENT_INDEX].instagram,
-        youtube: talent[TALENT_INDEX].youtube,
-        presskit: talent[TALENT_INDEX].presskit,
-      }}
-      />
-  </Layout>
-)
+	<Layout>
+		<TalentPage
+			{...{
+				title: talent[TALENT_INDEX].text,
+				name: talent[TALENT_INDEX].name,
+				image: talent[TALENT_INDEX].image,
+				stream: talent[TALENT_INDEX].stream,
+				instagram: talent[TALENT_INDEX].instagram,
+				youtube: talent[TALENT_INDEX].youtube,
+				presskit: talent[TALENT_INDEX].presskit,
+			}}
+		/>
+	</Layout>
+);
 
-export default ModalExamplePage
+export default ModalExamplePage;
